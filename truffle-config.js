fb5e2274c333fb0c2aa2a3962e2f01dec6cefc62
vladime88 <vladime@gmail.com>
2020-10-30 14:24:24 +0100

    rinkeby deploy

--- truffle-config.js
+++ truffle-config.js
@@ -1,46 +1,40 @@
-/**
- * Use this file to configure your truffle project. It's seeded with some
- * common settings for different networks and features like migrations,
- * compilation and testing. Uncomment the ones you need or modify
- * them to suit your project as necessary.
- *
- * More information about configuration can be found at:
- *
- * trufflesuite.com/docs/advanced/configuration
- *
- * To deploy via Infura you'll need a wallet provider (like @truffle/hdwallet-provider)
- * to sign your transactions before they're sent to a remote public node. Infura accounts
- * are available for free at: infura.io/register.
- *
- * You'll also need a mnemonic - the twelve word phrase the wallet uses to generate
- * public/private key pairs. If you're publishing your code to GitHub make sure you load this
- * phrase from a file you've .gitignored so it doesn't accidentally become public.
- *
- */
+/* eslint-disable camelcase */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
+const mnemonic = process.env.MNEMONIC;
+const projectId = process.env.ENDPOINT_ID;
+
 module.exports = {
-  // Configure networks
   networks: {
     development: {
-      host: '127.0.0.1',
-      port: 8545,
-      // eslint-disable-next-line camelcase
-      network_id: '*', // Match any network id
+      host: '127.0.0.1', // Localhost (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: '*', // Any network (default: none)
+    },
+    rinkeby: {
+      provider: () =>
+        new HDWalletProvider(
+          mnemonic,
+          `https://rinkeby.infura.io/v3/${projectId}`,
+        ),
+      network_id: 4,
+      gas: 5500000,
+      confirmations: 0,
+      timeoutBlocks: 200,
+      skipDryRun: true,
     },
   },
-  // Configure MochaJS testing framework
+
+  // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000
   },
+
   // Configure your compilers
   compilers: {
     solc: {
-      version: 'native',
+      version: 'native', // Fetch exact version from solc-bin (default: truffle's version)
     },
   },
 };
